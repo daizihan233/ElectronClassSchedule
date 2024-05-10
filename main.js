3aae0e1c92fa66178625b9ab1b2b53af4d6d1bbe
daizihan233 <y066ydg5@duck.com>
2024-05-11 00:29:38 +0800

    feat(家长会横幅): 遇到家长会会自动修改为横幅

--- main.js
+++ main.js
@@ -66,29 +66,23 @@ app.whenReady().then(() => {
         win.webContents.send('getWeekIndex');
         if (store.get("isFromCloud", false)) {
             let scheduleConfigSync;
-            const { net } = require('electron')
-            const url = store.get('url', "https://class.khbit.cn/")
-            console.log(url)
-            const request = net.request(url)
             let stat = true
-            while (stat) {
-                try {
-                    request.on('response', (response) => {
-                        response.on('data', (chunk) => {
-                            scheduleConfigSync = JSON.parse(chunk.toString())
-                            console.log(scheduleConfigSync)
-                        })
-                        response.on('end', () => {
-                            console.log('No more data in response.')
-                            win.webContents.send("newConfig", scheduleConfigSync)
-                        })
+            setTimeout(function () {
+                const { net } = require('electron')
+                const url = store.get('url', "https://class.khbit.cn/")
+                const request = net.request(url)
+                request.on('response', (response) => {
+                    response.on('data', (chunk) => {
+                        scheduleConfigSync = JSON.parse(chunk.toString())
+                        console.log(scheduleConfigSync)
                     })
-                    request.end()
-                    stat = false
-                } catch (e) {
-                    console.log(e)
-                }
-            }
+                    response.on('end', () => {
+                        console.log('No more data in response.')
+                        win.webContents.send("newConfig", scheduleConfigSync)
+                    })
+                })
+                request.end()
+            }, 5000)
         }
     })
     const handle = win.getNativeWindowHandle();
